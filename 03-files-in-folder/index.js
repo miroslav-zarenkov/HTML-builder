2f029a4a8a5a7a734dcd22ccc7a5154384df589c
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2023-05-09 18:08:02 +0400

    fix: change logic of getting file name to allow dots in filename

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -10,14 +10,15 @@ fs.promises
     files.forEach((file) => {
       if (file.isFile()) {
         const fullName = file.name;
-        const nameParts = fullName.split(".");
-        const name = nameParts[0];
+        const lastIndex = fullName.lastIndexOf(".");
+        const name = fullName.slice(0, lastIndex);
         const extension = path.extname(fullName).substring(1);
         const filePath = path.join(folderPath, fullName);
         fs.stat(filePath, (err, stats) => {
           if (err) {
             throw err;
           }
+
           const fileSize = stats.size;
           const sizeInKb = (fileSize / 1024).toFixed(3);
           const finalString = `${name} - ${extension} - ${sizeInKb}kb\n`;
